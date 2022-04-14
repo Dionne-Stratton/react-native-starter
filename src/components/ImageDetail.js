3e71e61a3fe8efc99fd90121a9e627ea0a18f1b5
Dionne <mylife33@hotmail.com>
2022-04-13 22:04:47 -0500

    wip

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { View, Button, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-const ImageDetail = ({navigation}) => {
+const ImageDetail = ({navigation, title, pic}) => {
   return <View>
-  <Text style={styles.text}>Image detail</Text>
+  <Text style={styles.text} img={pic} >Image of {title}</Text>
    </View>
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
+    margin: 15,
   }
 });
 
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { View, Button, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import ImageDetail from '../components/ImageDetail';
+import beach from '../../assets/beach.jpg'
 
 const ImageScreen = ({navigation}) => {
   return <View>
-  <Text style={styles.text}>Image Sreen</Text>
-    <ImageDetail />
+  <Text style={styles.text}>Image Sreen:</Text>
+    <ImageDetail title="forest" />
+    <ImageDetail title="mountains" />
+    <ImageDetail title="beach" pic ={beach} />
    </View>
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
+    margin: 15,
   }
 });
 
