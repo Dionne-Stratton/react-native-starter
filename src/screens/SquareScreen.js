c2062ee1ba7fccc19a87d960e36e4dd74dea872a
Dionne <mylife33@hotmail.com>
2022-04-14 20:01:18 -0500

    wip

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,34 +1,52 @@
 import React, { useReducer } from "react";
-import { View, Button, Text, StyleSheet, FlatList } from "react-native";
+import { View, Button, Text, StyleSheet} from "react-native";
 import ColorCounter from "../components/ColorCounter";
 
 const COLOR_INCREMENT = 20;
 
 const reducer = (state, action) =>{
-    
+    switch (action.colorToChange) {
+      case 'red':
+        if (state.red + action.amount > 255 || state.red + action.amount <0) {
+          return state
+        }
+        return { ...state, red: state.red + action.amount};
+      case 'green':
+        if (state.green + action.amount > 255 || state.green + action.amount <0) {
+          return state
+        }
+        return { ...state, green: state.green + action.amount};
+      case 'blue':
+        if (state.blue + action.amount > 255 || state.blue + action.amount <0) {
+          return state
+        }
+        return { ...state, blue: state.blue + action.amount};
+      default:
+        return state
+    }
 }
 
 const SquareScreen = () => {
-
-    const [state, dispatch] = useReducer(reducer, {red:0, green:0, blue:0})
+  const [state, dispatch] = useReducer(reducer, {red:0, green:0, blue:0})
+  const { red, green, blue } = state
 
   return (
     <View>
       <ColorCounter
-        onUp={() => }
-        onDown={() => }
+        onUp={() => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT}) }
+        onDown={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT}) }
         counter={red}
         color="red"
       />
       <ColorCounter
-        onUp={() => }
-        onDown={() => }
+        onUp={() => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT}) }
+        onDown={() => dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT}) }
         counter={green}
         color="green"
       />
       <ColorCounter
-        onUp={() => }
-        onDown={() => }
+        onUp={() => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT}) }
+        onDown={() => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT}) }
         color="blue"
         counter={blue}
       />
