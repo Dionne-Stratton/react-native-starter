9462cabd14768c68b0156403d75ecc483f629d5e
Dionne Stratton <mylife33@hotmail.com>
2022-04-16 21:41:26 -0500

    wip

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,13 +1,31 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Text, StyleSheet, View, TextInput } from "react-native";
 
 const TextScreen = () => {
-  return <Text style={styles.text}>Text screen</Text>;
+  const [name, setName] = useState("Slim Shaddy!");
+  return (
+    <View>
+      <Text style={styles.text}>Enter name:</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
+      />
+      <Text style={styles.text}>My name is: {name} </Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 20,
+  },
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
 
